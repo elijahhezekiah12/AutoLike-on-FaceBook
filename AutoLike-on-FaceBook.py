61868700a638ba8f32780eb89c59c9e8ce859395
Pocky Yang <31677343+pockyyang@users.noreply.github.com>
2019-12-05 22:49:51 +0800

    Update AutoLike-on-FaceBook.py

--- AutoLike-on-FaceBook.py
+++ AutoLike-on-FaceBook.py
@@ -4,16 +4,17 @@ from selenium.webdriver.common.keys import Keys
 import time
 driver = webdriver.Firefox()
 driver.get("https://www.facebook.com/")
-# # # 输入账号密码
+# input email and password
 driver.find_element_by_name("email").send_keys("email")
 driver.find_element_by_name("pass").send_keys("password")
-# 模拟点击登录
+# click the login button
 driver.find_element_by_css_selector('input[value="登录"]').click()
 Like_Counter = 0
 for Like_Counter in range(0,100000000,1):
+#clike the Like Button
   driver.find_elements_by_css_selector('div[class="_666k"]')[Like_Counter].click()
   time.sleep(0.1)
   driver.execute_script("window.scrollBy(0,3000)")
   Like_Counter=Like_Counter+1
-# 生成登陆后快照
+# screenshot the login website
 driver.save_screenshot("renren.png")
